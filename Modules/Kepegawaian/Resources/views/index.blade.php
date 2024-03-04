1a4e3d8921ca280b3262716f36e1a6bfbd0dde94
Nur Cholik Hasyim <ncadvertise@gmail.com>
2024-03-04 08:01:24 +0700

    Tambah sistem modular, dan contoh modul kepegawaian

---
+++ Modules/Kepegawaian/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('kepegawaian::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('kepegawaian.name') !!}
+    </p>
+@endsection
