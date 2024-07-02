9ea6f36491178aa8119f9c727b47ee09e3db5b66
stefani <indistefani0629@gmail.com>
2024-07-02 23:30:22 +0700

    fix perencanaan seeder

--- Modules/Keuangan/Database/Seeders/PerencanaanModulKeuanganTableSeeder.php
+++ Modules/Keuangan/Database/Seeders/PerencanaanModulKeuanganTableSeeder.php
@@ -23,7 +23,7 @@ class PerencanaanModulKeuanganTableSeeder extends Seeder
                 'nama' => 'Pengadaan Bahan Praktikum - Jurusan Bisnis dan Informatika',
                 'kode' => '000031',
                 'sumber' => 'RM',
-                'anggaran' => '40306000',
+                'pagu' => '40306000',
                 'revisi' => 1,
                 'unit_id' => 74
             ],
@@ -31,7 +31,7 @@ class PerencanaanModulKeuanganTableSeeder extends Seeder
                 'nama' => 'Workshop Big Data Analysis using Python (Internal)',
                 'kode' => '000235',
                 'sumber' => 'PNP',
-                'anggaran' => '27088000',
+                'pagu' => '27088000',
                 'revisi' => 1,
                 'unit_id' => 74
             ],
@@ -39,7 +39,7 @@ class PerencanaanModulKeuanganTableSeeder extends Seeder
                 'nama' => 'Workshop Introduction to DevOps with Kubernetes',
                 'kode' => '000236',
                 'sumber' => 'PNP',
-                'anggaran' => '27087000',
+                'pagu' => '27087000',
                 'revisi' => 1,
                 'unit_id' => 74
             ],
@@ -47,7 +47,7 @@ class PerencanaanModulKeuanganTableSeeder extends Seeder
                 'nama' => 'Workshop System Analyst and Design',
                 'kode' => '000237',
                 'sumber' => 'PNP',
-                'anggaran' => '27088000',
+                'pagu' => '27088000',
                 'revisi' => 1,
                 'unit_id' => 74
             ],
@@ -55,7 +55,7 @@ class PerencanaanModulKeuanganTableSeeder extends Seeder
                 'nama' => 'Workshop Software Quality Assurance',
                 'kode' => '000238',
                 'sumber' => 'PNP',
-                'anggaran' => '18207000',
+                'pagu' => '18207000',
                 'revisi' => 1,
                 'unit_id' => 74
             ],
@@ -63,7 +63,7 @@ class PerencanaanModulKeuanganTableSeeder extends Seeder
                 'nama' => 'Sewa lisensi software',
                 'kode' => '000239',
                 'sumber' => 'PNP',
-                'anggaran' => '20000000',
+                'pagu' => '20000000',
                 'revisi' => 1,
                 'unit_id' => 74
             ],
@@ -71,7 +71,7 @@ class PerencanaanModulKeuanganTableSeeder extends Seeder
                 'nama' => 'Perbaikan Peralatan Laboratorium',
                 'kode' => '000240',
                 'sumber' => 'PNP',
-                'anggaran' => '27271000',
+                'pagu' => '27271000',
                 'revisi' => 1,
                 'unit_id' => 74
             ],
@@ -81,7 +81,7 @@ class PerencanaanModulKeuanganTableSeeder extends Seeder
                 'nama' => 'Pengadaan Bahan Praktikum Bahan Segar - Jurusan Teknik Mesin',
                 'kode' => '000028',
                 'sumber' => 'PNP',
-                'anggaran' => '20000000',
+                'pagu' => '20000000',
                 'revisi' => 1,
                 'unit_id' => 75
             ],
@@ -89,7 +89,7 @@ class PerencanaanModulKeuanganTableSeeder extends Seeder
                 'nama' => 'Pengadaan Bahan Praktikum -Jurusan Teknik Mesin',
                 'kode' => '000036',
                 'sumber' => 'PNP',
-                'anggaran' => '150000000',
+                'pagu' => '150000000',
                 'revisi' => 1,
                 'unit_id' => 75
             ],
@@ -97,7 +97,7 @@ class PerencanaanModulKeuanganTableSeeder extends Seeder
                 'nama' => 'Keikutsertaan Lomba Nasional KKCTBN 2024',
                 'kode' => '000223',
                 'sumber' => 'PNP',
-                'anggaran' => '10000000',
+                'pagu' => '10000000',
                 'revisi' => 1,
                 'unit_id' => 75
             ],
@@ -105,7 +105,7 @@ class PerencanaanModulKeuanganTableSeeder extends Seeder
                 'nama' => 'Keikutsertaan Lomba Nasional KMHE, KRI, KMLI 2024',
                 'kode' => '000224',
                 'sumber' => 'PNP',
-                'anggaran' => '10000000',
+                'pagu' => '10000000',
                 'revisi' => 1,
                 'unit_id' => 75
             ],
@@ -113,7 +113,7 @@ class PerencanaanModulKeuanganTableSeeder extends Seeder
                 'nama' => 'Perpanjangan lisensi 3 (tiga) software lab. Komputer',
                 'kode' => '000225',
                 'sumber' => 'PNP',
-                'anggaran' => '64100000',
+                'pagu' => '64100000',
                 'revisi' => 1,
                 'unit_id' => 75
             ],
